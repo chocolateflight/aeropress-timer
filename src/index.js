f7389bc1ec9a0bb2fe9acc8dd3798f75903f775e
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-10-07 16:32:06 +0200

    moved fonts and css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/normalize.css'
-import './css/roboto-font.css'
-import './css/variables.css'
-import './css/style.css'
+import './assets/css/normalize.css'
+import './assets/css/roboto-font.css'
+import './assets/css/variables.css'
+import './assets/css/style.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

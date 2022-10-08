d8ce2f55257132afc030962be1654193a2cf7a60
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-10-08 19:00:58 +0200

    initial formatting

--- src/assets/components/countdown/CircularTimer.js
+++ src/assets/components/countdown/CircularTimer.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  .circulartimer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20rem;
+    aspect-ratio: 1/1;
+    border: 20px solid red;
+    border-radius: 50%
+  }
+`;
 
 
 function CircularTimer() {
 
   return (
-    <div>
-      Circular Timer
+    <Wrapper>
+    <div className='circulartimer'>
+      <small>Stir back and forward 3 times</small>
     </div>
+    </Wrapper>
   );
 }
 

d859f8957340d58616edc4203d54772b16fa2f7c
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-10-09 12:22:50 +0200

    connected recipes to global context

--- src/assets/components/layout/MobileMenu.js
+++ src/assets/components/layout/MobileMenu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useAppContext } from '../../context/appContext';
 
 // styled component
 const Wrapper = styled.div`
@@ -19,23 +20,55 @@ const Wrapper = styled.div`
     transition: ease 0.2s all;
   }
 
-  a {
+  span {
     color: var(--primary-500);
   }
 
   .animate {
     transform: translate3d(800px, 0, 0);
   }
+
+  .selected {
+    color: var(--green-light);
+  }
 `;
 
 // functions
 function MobileMenu(props) {
+  const { selectRecipe } = useAppContext();
+
+  function selectionHandler(e) {
+    selectRecipe({
+      selectedRecipe: +e.target.id,
+    });
+  }
+
+  const selectedRecipe = useAppContext()['selectedRecipe'];
+
   return (
     <Wrapper>
       <div className={`mobilemenu ${props.class}`}>
-        <a href='#'>Recipe 1</a>
-        <a href='#'>Recipe 2</a>
-        <a href='#'>Recipe 3</a>
+        <span
+          id='1'
+          className={selectedRecipe === 1 ? 'selected' : ''}
+          onClick={selectionHandler}
+        >
+          Recipe 1
+        </span>
+        <span
+          id='2'
+          className={selectedRecipe === 2 ? 'selected' : ''}
+          onClick={selectionHandler}
+        >
+          Recipe 2
+        </span>
+        <span
+          id='3'
+          className={selectedRecipe === 3 ? 'selected' : ''}
+          onClick={selectionHandler}
+        >
+          Recipe 3
+        </span>
       </div>
     </Wrapper>
   );
--- src/assets/recipes/Recipes.js
+++ src/assets/recipes/Recipes.js
@@ -1,37 +1,78 @@
 import React from 'react'
+import { useAppContext } from '../context/appContext';
 
 ////////////////////////////////////////////////////////
 
-// TEMPORARY RECIPE
-const recipe = {
-  id: 1,
-  name: 'Recipe 1',
-  author: 'Autor 1 Name',
-  steps: {
-    1: {
-      duration: 10,
-      description: 'Do step 1',
-    },
-    2: {
-      duration: 20,
-      description: 'Do step 2',
-    },
-    3: {
-      duration: 5,
-      description: 'Do step 3',
-    },
+const recipes = {
+  0: {
+    id: 1,
+    name: 'Recipe 1',
+    author: 'Autor 1 Name',
+    steps: {
+      1: {
+        duration: 10,
+        description: 'Do step 1',
+      },
+      2: {
+        duration: 20,
+        description: 'Do step 2',
+      },
+      3: {
+        duration: 5,
+        description: 'Do step 3',
+      }
+    }
   },
-};
+  1: {
+    id: 2,
+    name: 'Recipe 2',
+    author: 'Autor 2 Name',
+    steps: {
+      1: {
+        duration: 15,
+        description: 'Do step 1',
+      },
+      2: {
+        duration: 25,
+        description: 'Do step 2',
+      },
+      3: {
+        duration: 10,
+        description: 'Do step 3',
+      }
+    }
+  },
+  2: {
+    id: 3,
+    name: 'Recipe 3',
+    author: 'Autor 3 Name',
+    steps: {
+      1: {
+        duration: 5,
+        description: 'Do step 1',
+      },
+      2: {
+        duration: 15,
+        description: 'Do step 2',
+      },
+      3: {
+        duration: 1,
+        description: 'Do step 3',
+      }
+    }
+  },
+}
 
-const durationArray = Object.values(recipe.steps).map((val) => val.duration);
-const descriptionArray = Object.values(recipe.steps).map((val) => val.description);
-const totalDuration = durationArray.reduce((partialSum, a) => partialSum + a, 0);
+// const stepsArray = Object.values(recipe.steps)
+// const durationArray = Object.values(recipe.steps).map((val) => val.duration);
+// const descriptionArray = Object.values(recipe.steps).map((val) => val.description);
+// const totalDuration = durationArray.reduce((partialSum, a) => partialSum + a, 0);
 
 ////////////////////////////////////////////////////////
 
 function Recipes() {
   return (
-    <div>Recipes</div>
+    <div>Hello</div>
   )
 }
 

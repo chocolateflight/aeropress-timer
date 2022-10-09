4e52cc637c3ce14c7fc1f3d6b90141b5ef9975c4
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-10-09 12:22:29 +0200

    remove temporary recipe

--- src/assets/components/countdown/NumericTimer.js
+++ src/assets/components/countdown/NumericTimer.js
@@ -9,37 +9,10 @@ const Wrapper = styled.div`
   }
 `;
 
-////////////////////////////////////////////////////////
 
-// TEMPORARY RECIPE
-const recipe = {
-  id: 1,
-  name: 'Recipe 1',
-  author: 'Autor 1 Name',
-  steps: {
-    1: {
-      duration: 10,
-      description: 'Do step 1',
-    },
-    2: {
-      duration: 20,
-      description: 'Do step 2',
-    },
-    3: {
-      duration: 5,
-      description: 'Do step 3',
-    },
-  },
-};
-
-const durationArray = Object.values(recipe.steps).map((val) => val.duration);
-const descriptionArray = Object.values(recipe.steps).map((val) => val.description);
-const totalDuration = durationArray.reduce((partialSum, a) => partialSum + a, 0);
-
-////////////////////////////////////////////////////////
 
 function NumericTimer(props) {
-  const [time, setTime] = useState(totalDuration); // remember to change
+  const [time, setTime] = useState(10); // remember to change
 
   useEffect(() => {
     let interval = null;

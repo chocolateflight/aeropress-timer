4d1ee570d8822664af397bb5d95707915e38eb92
Marc <67051612+chocolateflight@users.noreply.github.com>
2022-10-10 09:39:24 +0200

    updated recipes

--- src/assets/recipes/Recipes.js
+++ src/assets/recipes/Recipes.js
@@ -1,58 +1,86 @@
 const recipes = {
   1: {
     id: 1,
-    name: 'Recipe 1',
-    author: 'Autor 1 Name',
+    name: 'Ultimate AeroPress Recipe',
+    author: 'James Hoffmann',
     steps: {
       1: {
-        duration: 10,
-        description: 'Do step 1',
+        duration: 120,
+        description: 'Pour 200g of hot water over the grounds. Insert plunger at the top and lift up a bit to create a seal.',
       },
       2: {
-        duration: 20,
-        description: 'Do step 2',
+        duration: 30,
+        description: 'Give the cup and brew chamber a swirl, then let sit for a while longer',
       },
       3: {
-        duration: 5,
-        description: 'Do step 3',
+        duration: 30,
+        description: 'Press gently into the cup, all the way through the hissing.',
       },
     },
   },
   2: {
     id: 2,
-    name: 'Recipe 2',
-    author: 'Autor 2 Name',
+    name: 'WAC 2021 Winner',
+    author: 'Tuomas Merikanto',
     steps: {
       1: {
-        duration: 15,
-        description: 'Do step 1',
+        duration: 10,
+        description: 'Pour 50g of hot water over grounds',
       },
       2: {
-        duration: 25,
-        description: 'Do step 2',
+        duration: 5,
+        description: 'Stir gently 3 times back and forth',
       },
       3: {
+        duration: 15,
+        description: 'Pour another 1150g of hot water',
+      },
+      4: {
+        duration: 20,
+        description: 'Let it sit',
+      },
+      5: {
         duration: 10,
-        description: 'Do step 3',
+        description: 'Stir gently 3 times back and forth',
+      },
+      6: {
+        duration: 40,
+        description: 'Push out any excess air and attach filter cap. Let it sit.',
+      },
+      7: {
+        duration: 20,
+        description: 'Flip and press into pitcher',
+      },
+      9: {
+        duration: 30,
+        description: 'Swirl the pitcher',
+      },
+      10: {
+        duration: 10,
+        description: 'Pour from altitude into another pitcher.',
       },
     },
   },
   3: {
     id: 3,
-    name: 'Recipe 3',
-    author: 'Autor 3 Name',
+    name: 'Original Recipe',
+    author: 'Alan Adler',
     steps: {
       1: {
-        duration: 3,
-        description: 'Do step 1',
+        duration: 15,
+        description: 'Pour water to about 1 1/2 on the champer (95g)',
       },
       2: {
-        duration: 3,
-        description: 'Do step 2',
+        duration: 10,
+        description: 'Stir using the paddle',
       },
       3: {
-        duration: 3,
-        description: 'Do step 3',
+        duration: 30,
+        description: 'Insert plunger and press down all the way to reaching the grounds',
+      },
+      4: {
+        duration: 20,
+        description: 'Add additional hot water to the cup until you like it',
       },
     },
   },
